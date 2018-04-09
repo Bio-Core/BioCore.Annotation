788b5b69ad8f24b4dc444ac9c9d2918fe24c584d
Richard de Borja <richard.deborja@uhnresearch.ca>
2018-04-09 15:50:11 -0400

    remove printout of which file is being processed

--- R/create_merged_data.R
+++ R/create_merged_data.R
@@ -4,7 +4,6 @@ create_merged_data <- function(path=NULL, pattern='.hg19_multianno.txt', colhead
   files <- get_annovar_files(path=path, pattern=pattern, recursive=FALSE)
   data <- data.frame()
   for(i in 1:length(files)) {
-    print(paste(sep=' ', "Processing file", files[i], "..."))
     tmp_data <- load_annovar_file(file=files[i], colheader=colheader)
     tmp_data <- add_variant_allele_fraction_column(data=tmp_data)
     tmp_data <- add_read_depth_column(data=tmp_data)
