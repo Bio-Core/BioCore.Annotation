153bda09a64b1d49ddbbd41dc51b54050e86d628
Richard de Borja <richard.deborja@uhnresearch.ca>
2018-04-12 11:13:46 -0400

    added package info to filter calls

--- R/get_snv.R
+++ R/get_snv.R
@@ -2,7 +2,7 @@ get_snv <- function(data=NULL) {
   if (is.null(data)) stop("Mandatory argument data is missing")
 
   # remove all indels
-  data <- data %>% filter(nchar(Ref) < 2 & nchar(Alt) < 2)
-  data <- data %>% filter(Ref != '-' & Alt != '-')
+  data <- data %>% dplyr::filter(nchar(Ref) < 2 & nchar(Alt) < 2)
+  data <- data %>% dplyr::filter(Ref != '-' & Alt != '-')
   return(data)
 }
