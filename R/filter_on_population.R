0fee963af9162d802e9db81252fab3ab67f54ed6
Richard de Borja <richard.deborja@uhnresearch.ca>
2018-04-25 15:39:09 -0400

    added checks to see if column names of population annotations exists

--- R/filter_on_population.R
+++ R/filter_on_population.R
@@ -1,11 +1,19 @@
 filter_on_population <- function(data=NULL, exac_threshold=0.01, g1000_threshold=0.01, esp_threshold=0.01) {
   if (is.null(data)) stop("Mandatory argument data is missing")
-  data$ExAC_ALL <- as.numeric(data$ExAC_ALL)
-  data$`1000G_ALL` <- as.numeric(data$`1000G_ALL`)
-  data$ESP6500siv2_ALL <- as.numeric(data$ESP6500siv2_ALL)
-  filtered_pop_data <- data %>%
-    filter(ExAC_ALL < exac_threshold) %>%
-    filter(ESP6500siv2_ALL < esp_threshold) %>%
-    filter(`1000G_ALL` < g1000_threshold)
-  return(filtered_pop_data)
+
+  # check to see if the columns exist then filter if they do
+  if ("ExAC_ALL" %in% colnames(data)) {
+    data$ExAC_ALL <- as.numeric(data$ExAC_ALL)
+    data <- data %>% filter(ExAC_ALL < exac_threshold)
+  }
+  if ("1000G_ALL" %in% colnames(data)) {
+    data$`1000G_ALL` <- as.numeric(data$`1000G_ALL`)
+    data <- data %>% filter(`1000G_ALL` < g1000_threshold)
+  }
+  if ("ESP6500siv2_ALL" %in% colnames(data)) {
+    data$ESP6500siv2_ALL <- as.numeric(data$ESP6500siv2_ALL)
+    data <- data %>% filter(ESP6500siv2_ALL < esp_threshold)
+  }
+
+  return(data)
 }
