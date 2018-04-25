d4be2f4221f5f22a4a25aa63290b3a478f0d05d2
Richard de Borja <richard.deborja@uhnresearch.ca>
2018-04-25 18:53:29 -0400

    added dplyr:: to filter statememts

--- R/filter_on_population.R
+++ R/filter_on_population.R
@@ -4,15 +4,15 @@ filter_on_population <- function(data=NULL, exac_threshold=0.01, g1000_threshold
   # check to see if the columns exist then filter if they do
   if ("ExAC_ALL" %in% colnames(data)) {
     data$ExAC_ALL <- as.numeric(data$ExAC_ALL)
-    data <- data %>% filter(ExAC_ALL < exac_threshold)
+    data <- data %>% dplyr::filter(ExAC_ALL < exac_threshold)
   }
   if ("1000G_ALL" %in% colnames(data)) {
     data$`1000G_ALL` <- as.numeric(data$`1000G_ALL`)
-    data <- data %>% filter(`1000G_ALL` < g1000_threshold)
+    data <- data %>% dplyr::filter(`1000G_ALL` < g1000_threshold)
   }
   if ("ESP6500siv2_ALL" %in% colnames(data)) {
     data$ESP6500siv2_ALL <- as.numeric(data$ESP6500siv2_ALL)
-    data <- data %>% filter(ESP6500siv2_ALL < esp_threshold)
+    data <- data %>% dplyr::filter(ESP6500siv2_ALL < esp_threshold)
   }
 
   return(data)
