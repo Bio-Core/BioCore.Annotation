a5cb4444936e84e89569a47921650b092d7673f3
Richard de Borja <richard.deborja@uhnresearch.ca>
2020-02-03 10:36:39 -0500

    added arguments to define the columns for each of the population columns to filter

--- R/filter_on_population.R
+++ R/filter_on_population.R
@@ -1,18 +1,24 @@
-filter_on_population <- function(data=NULL, exac_threshold=0.01, g1000_threshold=0.01, esp_threshold=0.01) {
+filter_on_population <- function(data=NULL,
+                                 exac_threshold=0.01,
+                                 g1000_threshold=0.01,
+                                 esp_threshold=0.01,
+                                 exac="ExAC_ALL",
+                                 g1000="X1000G_ALL",
+                                 esp="ESP6500siv2_ALL") {
   if (is.null(data)) stop("Mandatory argument data is missing")
 
   # check to see if the columns exist then filter if they do
-  if ("ExAC_ALL" %in% colnames(data)) {
-    data$ExAC_ALL <- as.numeric(data$ExAC_ALL)
-    data <- data %>% dplyr::filter(ExAC_ALL < exac_threshold)
+  if (exac %in% colnames(data)) {
+    data[[exac]] <- as.numeric(data[[exac]])
+    data <- data %>% dplyr::filter(!!as.name(exac) < exac_threshold)
   }
-  if ("1000G_ALL" %in% colnames(data)) {
-    data$`1000G_ALL` <- as.numeric(data$`1000G_ALL`)
-    data <- data %>% dplyr::filter(`1000G_ALL` < g1000_threshold)
+  if (g1000 %in% colnames(data)) {
+    data[[g1000]] <- as.numeric(data[[g1000]])
+    data <- data %>% dplyr::filter(!!as.name(g1000) < g1000_threshold)
   }
-  if ("ESP6500siv2_ALL" %in% colnames(data)) {
-    data$ESP6500siv2_ALL <- as.numeric(data$ESP6500siv2_ALL)
-    data <- data %>% dplyr::filter(ESP6500siv2_ALL < esp_threshold)
+  if (esp %in% colnames(data)) {
+    data[[esp]] <- as.numeric(data[[esp]])
+    data <- data %>% dplyr::filter(!!as.name(esp) < esp_threshold)
   }
 
   return(data)
